09ad87eecc18bf0440a86a8290fe2704e5017b37
John Beale <7452231+jbeale1@users.noreply.github.com>
2024-02-11 13:53:18 -0800

    use two subplots

--- plot-stack.py
+++ plot-stack.py
@@ -12,7 +12,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot_all_csv(directory_path):
-    csv_files = glob.glob(os.path.join(directory_path, '*.csv'))
+    csv_files = glob.glob(os.path.join(directory_path, '*_spec.csv'))
 
     if not csv_files:
         print(f"No .csv files in directory: {directory_path}")
@@ -32,25 +32,51 @@ def plot_all_csv(directory_path):
         print("No valid data found in files.")
         return
 
-    combined_df = pd.concat(dfs, axis=1) # combine and plot all frames
-    combined_df.plot()
-
-    plt.title('stacked spectrums')
-    plt.xlabel('channel')
-    plt.ylabel('counts')
-    plt.gca().get_legend().remove()
-    plt.yscale('log')
-    plt.grid(visible=True, linestyle="dotted", color="gray")
+    df = pd.concat(dfs, axis=1)  # combine and plot all frames
+    hours = len(df.columns)      # each column was accumulated over 1 hour
+    df['mean'] = df.mean(axis=1) # create new column with row-average of all existing cols
+    
+    fig, (ax1,ax2) = plt.subplots(nrows=2, ncols=1)        
+        
+    # print(df['mean'])
+    df['mean'].plot(ax=ax1)
+    #plt.plot(df['mean'])
+    
+    sTitle = ("Spectrum Avg: %d hours" % hours)
+    ax1.set_title(sTitle)
+    ax1.set_xlabel('channel')
+    #ax1.set_ylabel('counts')
+    ax1.set_yscale('log')
+    ax1.grid(visible=True, linestyle="dotted", color="gray")
+    ax1.axis(ymin=1)
+    #plt.show()
+    #return
+    
+    df.plot(ax=ax2)    
+    #sTitle = ("Spectrum Sequence: %d hours" % hours)
+    #ax2.set_title(sTitle)
+    ax2.set_xlabel('channel')
+    ax2.set_ylabel('counts')
+    ax2.set_yscale('log')
+    ax2.grid(visible=True, linestyle="dotted", color="gray")
+    ax2.axis(ymin=1)
+    ax2.get_legend().remove()
+   
     plt.show()
 
 # ----------------------------------------------------------------------
 if __name__ == "__main__":
         
+    print("Plot average of csv files v0.1")
+    dir = ""
     if len(sys.argv) != 2:
         print("Usage: plot-stack.py <directory>")
-    else:
-        try:
+        dir = os.path.dirname(os.path.realpath(__file__))
+    
+    try:
+        if (dir == ""):
             dir = sys.argv[1]
-            plot_all_csv(dir)
-        except Exception as e:
-            print(f"Error: {e}")
+        print("Reading %s" % dir)
+        plot_all_csv(dir)
+    except Exception as e:
+        print(f"Error: {e}")
